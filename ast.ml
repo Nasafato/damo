43e68bdad884a275de0fddba6249f20d50aeb57d
Alan Gou <alanjgou@gmail.com>
2017-03-25 15:18:11 -0700

    Initialize AST file with one line to start
    We'll be filling in the rest as we go along, but starting only
    with the most basic operators.

---
+++ ast.ml
@@ -0,0 +1,15 @@
+(* Abstract Syntax Tree and functions for printing it *)
+
+type op = 
+    Add 
+  | Sub 
+  | Mult 
+  | Div
+  | Equal 
+  | Neq 
+  | Less 
+  | Leq
+  | Greater
+  | Geq
+  | And
+  | Or
